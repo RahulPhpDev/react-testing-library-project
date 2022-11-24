145ae9a0eb917f73a4a994b3f5eb3916380466d6
Rahul Chauhan <mrrahul2016@gmail.com>
2022-11-24 13:35:42 +0530

    click event

--- src/components/counter/counter.test.tsx
+++ src/components/counter/counter.test.tsx
@@ -1,15 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
 import { Counter } from './counter';
 
 
 describe('Counter', () => {
+
+    // beforeEach( () => {
+    //     render(<Counter />);
+    // })
     it('test it render correctly', () => {
         render(<Counter />);
 
         const counterElement = screen.getByRole('heading');
-
         expect(counterElement).toBeInTheDocument();
-
         const incrementButton = screen.getByRole('button', {
             name: "Increment"
         });
@@ -19,9 +22,57 @@ describe('Counter', () => {
 
     test('render a count to 0', () => {
         render(<Counter />);
-
         const counterElement = screen.getByRole("heading");
-
         expect(counterElement).toHaveTextContent(0);
-    })
+    });
+
+    test('increase counter after click button', async() => {
+       user.setup();
+        render(<Counter />);
+        const incrementButton  =   screen.getByRole("button", {
+            name: "Increment"
+        });
+       await user.click(incrementButton);
+       const counterElement = screen.getByRole("heading");
+       expect(counterElement).toHaveTextContent('1');
+    });
+
+    test('render input type for set count', () => {
+        render( <Counter />);
+        
+        const inputNode = screen.getByRole('textbox');
+        expect(inputNode).toBeInTheDocument();
+    });
+
+    test('render a set button', () => {
+        render(<Counter />);
+
+        const buttonRole = screen.getByRole('button', {
+            name: 'Set'
+        });
+
+        expect(buttonRole).toBeInTheDocument();
+    });
+
+    test('set by 10 after have value in input and click set button' , async() => {
+        user.setup();
+        render(<Counter />);
+        const inputNode = screen.getByRole('textbox');
+        const inputValue = '10';
+      await  user.type(inputNode, inputValue)
+        // fireEvent.change(inputNode, {target: {value: 10}});
+        expect(inputNode).toHaveValue(inputValue);
+
+        const buttonElement = screen.getByRole('button', {
+            name: /set/i
+        })
+        expect(buttonElement).toBeInTheDocument();
+        await user.click(buttonElement);
+        const counterElement = screen.getByRole("heading");
+       expect(counterElement).toHaveTextContent(inputValue);
+
+
+
+    });
+
 })
--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -2,11 +2,14 @@ import { useState } from "react"
 
 export const Counter  = () => {
     const [ count, setCount] = useState(0);
-
+    const [amount, setAmount] = useState(0);
     return (
         <div>
               <h1> {count} </h1>  
               <button onClick={() => setCount(count+ 1)} > Increment </button>
+              <input type = "text" value = {amount} onChange = { (e) => setAmount(+e?.target?.value) }/>
+              {/* onClick = {() => setCount(amount)} */}
+              <button type = 'button' onClick = {() => setCount(amount)} >Set</button>
         </div>
     );
 
